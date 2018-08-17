a1595052631e0f9415ce6f2f1eb0c4015cab12b5
Pedro Caleia <pmig.caleia@gmail.com>
2018-08-17 19:32:06 +0100

    Initial commit

---
+++ src/test/java/pt/pcaleia/FooBarTest.java
@@ -0,0 +1,21 @@
+package pt.pcaleia;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * @author Pedro Caleia
+ */
+public final class FooBarTest {
+	
+	
+	@Test
+	public void testMultiply() {
+		FooBar fooBar = new FooBar( 10, 20 );
+		
+		Assertions.assertEquals( 200, fooBar.multiply() );
+	}
+
+}
